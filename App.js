ff516dcd5a303a7defd1f4d6b0c836ba3e61fab0
Aaron Diggdon <aarondiggdon@gmail.com>
2021-11-18 13:53:43 -0800

    connected to api

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { response } from "express";
 import React, {useState} from "react";
 import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native";
 import Return from "./components/profileReturns/index";
@@ -7,9 +6,23 @@ import Return from "./components/profileReturns/index";
 
 export default function App() {
 const [data, setData] = useState();
-
+const [loading, setLoading] = useState(true);
 const requestData = async () => {
- 
+  try {
+    console.log("PRESS")
+    const response = await fetch('https://api.propublica.org/congress/v1/117/senate/members.json', {
+      method: 'GET',
+      headers: {
+        "X-Api-Key": "boSbGSKgKnoMtdwEjxT0a8Bymy6LaOIaJGWAX0dY "
+      }
+    })
+    const json = await response.json();
+    setData(json)
+  } catch(error) {
+    console.log("Nothing In Return")
+  } finally {
+    setLoading(false);
+  }
  
   
 
